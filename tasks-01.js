d2e810db3eeaa494666f74994f661857ad5ff9fe
Oleksandra Shkoda <shkoda.aleksandra@gmail.com>
2022-09-21 17:52:25 +0100

    save

---
+++ tasks-01.js
@@ -0,0 +1,25 @@
+// task 1  Shkoda
+function makeNegative(num){
+    return (num <= 0)?num:num*-1;
+  }
+//   task 2 Shkoda
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return +laLigaGoals + +copaDelReyGoals+ +championsLeagueGoals;
+   }
+// task 3 Shkoda
+function move (position, roll) {
+    return position+ +roll*2;
+  }
+  //task 4 Shkoda
+  function greet (name, owner) {
+    return (name===owner)?"Hello boss":"Hello guest";
+   }
+   //task 5 Shkoda
+   function litres(time){
+    return Math.floor(0.5*time);
+  }
+  //task 6 Shkoda
+  function lovefunc(flower1, flower2){
+    return(+flower1%2 !== +flower2%2)?true:false;
+  }
+
